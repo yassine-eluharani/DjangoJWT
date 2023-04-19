998841d821bf5a6392f1568105fa44d403eae500
Eluharni yassine <eluharni.yassine@gmail.com>
2023-04-19 22:34:07 +0000

    Added django framework

--- backend/base/api/views.py
+++ backend/base/api/views.py
@@ -1,5 +1,10 @@
 from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
+
+#This decerator takes a list of http methods we ca send 
+@api_view(['GET'])
 def getRoutes(request):
     routes = [
         # This endpoint is where we submit the username and password and we get back an access token
@@ -7,4 +12,4 @@ def getRoutes(request):
         # This will give us a new token based on a refresh token that you sent to the backend 
         '/api/token/refresh'
     ]
-    return JsonResponse(routes, safe= False)
+    return Response(routes)
