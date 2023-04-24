2d69b5d968b3897936efd83d8c50d465ed5fe353
Eluharni yassine <eluharni.yassine@gmail.com>
2023-04-24 19:15:23 +0000

    Created Note model

--- backend/base/api/views.py
+++ backend/base/api/views.py
@@ -2,8 +2,11 @@ from django.http import JsonResponse
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer, serializers
 from rest_framework_simplejwt.views import TokenObtainPairView
+from base.api.serializers import NoteSerializer
+
+from base.models import Note
 
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
@@ -28,5 +31,12 @@ def getRoutes(request):
         '/api/token/refresh'
     ]
     return Response(routes)
+
+
+@api_view(['GET'])
+def getNotes(request):
+    notes = Note.objects.all()
+    serializer = NoteSerializer(notes, many = True)
+    return Response(serializer.data)
 
 
