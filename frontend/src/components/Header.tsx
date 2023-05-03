c19f6ff107aa86baf7e8aea9dd08fc891f6a92be
Eluharni yassine <eluharani.yassine@gmail.com>
2023-05-03 13:42:31 +0100

    Cart items in header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -4,33 +4,40 @@ import AuthContext from "../context/AuthContext";
 
 const Header = () => {
   const { user, logout, authTokens } = useContext(AuthContext);
-  const [cart, setCart] = useState({});
+  const [cart, setCart] = useState([]);
   useEffect(() => {
-    getCart();
-    console.log(cart);
-  }, []);
-  const getCart = async () => {
-    const response = await fetch("http://localhost:8000/product_api/getCart", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Bearer " + String(authTokens.access),
-      },
-    });
-    const data = await response.json();
-
-    if (response.status === 200) {
-      setCart(data);
-      console.log(cart);
-    } else if (response.statusText === "Unauthorized") {
-      logout();
+    async function getCart() {
+      try {
+        const response = await fetch(
+          "http://localhost:8000/product_api/getCart/",
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: "Bearer " + String(authTokens.access),
+            },
+          }
+        );
+        if (response.ok) {
+          const data = await response.json();
+          setCart(data.products);
+          console.log(cart);
+        } else if (response.status === 401) {
+          logout();
+        } else {
+          // handle other errors
+        }
+      } catch (error) {
+        console.error(error);
+      }
     }
-  };
+    getCart();
+  }, [authTokens]);
 
   return (
-    <div>
+    <div className="flex">
       <Link to="/">Home</Link>
-      <span>|</span>
+      <span> |</span>
       {user ? (
         <button
           className="bg-transparent hover:bg-red-500 text-red-700 font-semibold hover:text-white py-2 px-4 border border-red-500 hover:border-transparent rounded"
@@ -41,6 +48,9 @@ const Header = () => {
       ) : (
         <Link to="/login">Login</Link>
       )}
+
+      <span> |</span>
+      {cart && <h3>Cart Items: {cart.length}</h3>}
     </div>
   );
 };
